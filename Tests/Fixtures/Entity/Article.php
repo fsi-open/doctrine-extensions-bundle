f6e74a79b8baa3171b43107fe469a77f580f5daf
Norbert Orzechowicz <norbert.orzechowicz@fsi.pl>
2013-07-15 11:58:27 +0200

    Added fsi_file form type

---
+++ Tests/Fixtures/Entity/Article.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * (c) Fabryka Stron Internetowych sp. z o.o <info@fsi.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FSi\Bundle\DoctrineExtensionsBundle\Tests\Fixtures\Entity;
+
+use FSi\DoctrineExtensions\Uploadable\File;
+
+class Article
+{
+    /**
+     * @var \FSi\DoctrineExtensions\Uploadable\File
+     */
+    protected $file;
+
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    public function setFile(File $file)
+    {
+        $this->file = $file;
+    }
+}
