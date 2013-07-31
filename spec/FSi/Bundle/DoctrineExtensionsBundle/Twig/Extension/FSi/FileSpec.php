eadf1d4182a05e7e9f1151a737e5144db98bad03
Norbert Orzechowicz <norbert.orzechowicz@fsi.pl>
2013-07-31 16:46:16 +0200

    Added twig function is_fsi_file

---
+++ spec/FSi/Bundle/DoctrineExtensionsBundle/Twig/Extension/FSi/FileSpec.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace spec\FSi\Bundle\DoctrineExtensionsBundle\Twig\Extension\FSi;
+
+use FSi\DoctrineExtensions\Uploadable\File;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class FileSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('FSi\Bundle\DoctrineExtensionsBundle\Twig\Extension\FSi\File');
+    }
+
+    function it_is_twig_extension()
+    {
+        $this->shouldBeAnInstanceOf('Twig_Extension');
+    }
+
+    function it_have_fsi_url_name()
+    {
+        $this->getName()->shouldReturn('fsi_file');
+    }
+
+    function it_have_is_fsi_file_function()
+    {
+        $this->getFunctions()->shouldHaveFunction('is_fsi_file');
+    }
+
+    function it_recognize_fsi_file(File $file)
+    {
+        $this->isFSiFile('thisisnotfile')->shouldReturn(false);
+        $this->isFSiFile(new \DateTime())->shouldReturn(false);
+        $this->isFSiFile($file)->shouldReturn(true);
+    }
+
+    public function getMatchers()
+    {
+        return array(
+            'haveFunction' => function($subject, $key) {
+                foreach ($subject as $function) {
+                    if ($function instanceof \Twig_SimpleFunction) {
+                        if ($function->getName() == $key) {
+                            return true;
+                        }
+                    }
+                }
+
+                return false;
+            },
+        );
+    }
+}
