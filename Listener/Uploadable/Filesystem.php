bbf041037d39dd13173927e9b8988f3aec00a139
Lukasz Cybula <lukasz@fsi.pl>
2015-02-10 14:02:00 +0100

    Added fsi_file_url twig function and additional bundle configuration

---
+++ Listener/Uploadable/Filesystem.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * (c) FSi sp. z o.o. <info@fsi.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FSi\Bundle\DoctrineExtensionsBundle\Listener\Uploadable;
+
+class Filesystem extends \Gaufrette\Filesystem
+{
+    /**
+     * @var string
+     */
+    protected $baseUrl;
+
+    /**
+     * @param string $baseUrl
+     */
+    public function setBaseUrl($baseUrl)
+    {
+        $this->baseUrl = $baseUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBaseUrl()
+    {
+        return $this->baseUrl;
+    }
+}
