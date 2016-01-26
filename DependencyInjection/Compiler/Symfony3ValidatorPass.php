d1c05c34d35ae1e8dce0f6dd75a7874e700efba4
Lukasz Cybula <lukasz@fsi.pl>
2016-01-26 14:07:37 +0100

    Fix for symfony < 3.0

--- DependencyInjection/Compiler/Symfony3ValidatorPass.php
+++ DependencyInjection/Compiler/Symfony3ValidatorPass.php
@@ -10,7 +10,7 @@ final class Symfony3ValidatorPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        if (version_compare(Kernel::VERSION, '3.0.0', '<')) {
+        if (version_compare(Kernel::VERSION, '3.0.0', '>=')) {
             $container->findDefinition('fsi_doctrine_extensions.validator.file')
                 ->setClass('FSi\Bundle\DoctrineExtensionsBundle\Validator\Constraints\Symfony3\FileValidator');
 
