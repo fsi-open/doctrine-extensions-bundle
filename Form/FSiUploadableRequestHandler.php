aafc791f7054781f714ddc8361dbde7cef93a2e2
Lukasz Cybula <lukasz@fsi.pl>
2018-01-24 13:10:55 +0100

    Fix for Symfony >= 2.8.30

---
+++ Form/FSiUploadableRequestHandler.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * (c) FSi sp. z o.o. <info@fsi.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FSi\Bundle\DoctrineExtensionsBundle\Form;
+
+use FSi\DoctrineExtensions\Uploadable\File;
+use Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationRequestHandler as BaseHttpFoundationRequestHandler;
+
+class FSiUploadableRequestHandler extends BaseHttpFoundationRequestHandler
+{
+    public function isFileUpload($data)
+    {
+        return parent::isFileUpload($data) || ($data instanceof File);
+    }
+}
